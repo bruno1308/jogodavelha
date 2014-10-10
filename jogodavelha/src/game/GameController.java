466627200f7d4c64cd07541ab0d3ee3637167fd3
Bruno Fernandes Lopes <brunofernandeslopes@gmail.com>
2014-10-10 16:52:01 -0700

    fulvio guay

--- jogodavelha/src/game/GameController.java
+++ jogodavelha/src/game/GameController.java
@@ -9,22 +9,54 @@ package game;
  * @author Bruno
  */
 public class GameController {
-    public char[][] board = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    public int turn;
+    public char[][] board = {{'e','e','e'},{'e','e','e'},{'e','e','e'}};
+    public boolean turn;
     GameController(int player){
-        this.turn = player;        
+        if(player == 0){
+          this.turn = true;  
+        }        
+        else {
+            this.turn = false;
+        }
     }
     
     public boolean checkWinner(){
         return true;
     }
     
-    public void setMove(int[] pos){
-        if(this.turn == 0) board[pos[0]][pos[1]] = 'X';
-        else board[pos[0]][pos[1]] = 'O';        
+    public boolean isValid(int[] pos){
+        if(pos[0] >= 0 && pos[0] < 3 && pos[1] >=0 && pos[1] < 3 && board[pos[0]][pos[1]] == 'e'){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean setMove(int[] pos){
+        if(isValid(pos)){
+        if(this.turn == true) {
+            board[pos[0]][pos[1]] = 'X';
+        }
+        else {
+            board[pos[0]][pos[1]] = 'O';
+        }  
+        turn = !turn;
+        return true;
+        }
+        else{
+            return false;
+        }
     }
     
     public void printBoard(){
-        System.out.println(board);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if(j!=2)
+                    System.out.print("|");
+            }
+            if(i!=2)
+            System.out.print("\n-|-|-\n");
+        }
+        
     }
 }
---
+++ jogodavelha/src/game/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Bruno
+ */
+public class main {
+    public static void main(String[] args) {
+        GameController gc = new GameController(0);
+        gc.printBoard();
+        System.out.println("\n\n");
+        int move[] = {0,0};
+        gc.setMove(move);
+        gc.setMove(move);
+        gc.printBoard();
+    }
+}
