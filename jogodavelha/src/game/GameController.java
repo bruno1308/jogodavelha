d0b81cd10ccdeb60e45061ab56e9ebe2c47b9513
Bruno Fernandes Lopes <brunofernandeslopes@gmail.com>
2014-10-10 17:40:58 -0700

    Check

--- jogodavelha/src/game/GameController.java
+++ jogodavelha/src/game/GameController.java
@@ -19,13 +19,21 @@ public class GameController {
             this.turn = false;
         }
     }
-    public boolean checkHorizontal(){
-        return true;
+    public int checkWinner(){
         
-    }
-    public boolean checkWinner(){
-        if(board[0][0] == 'X') return true;
-        return true;
+        for (int i = 0; i < 3; i++) {
+            if(board[i][0] == 'X' && board[i][1] == 'X' &&board[i][2] == 'X') return 0;
+            if(board[0][i] == 'X' && board[1][i] == 'X' &&board[2][i] == 'X') return 0;
+            if(board[i][0] == 'O' && board[i][1] == 'O' &&board[i][2] == 'O') return 1;
+            if(board[0][i] == 'O' && board[1][i] == 'O' &&board[2][i] == 'O') return 1;
+        }
+        if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') return 0;
+        if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') return 1;
+        if(board[0][3] == 'X' && board[1][1] == 'X' && board[3][0] == 'X') return 0;
+        if(board[0][3] == 'O' && board[1][1] == 'O' && board[3][0] == 'O') return 1;
+
+        
+        return -1;
     }
     
     public boolean isValid(int[] pos){
